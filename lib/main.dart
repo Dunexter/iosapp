b87511e78e78d53878b4700e9c42b6760bf43c21
unknown <parthptl44@gmail.com>
2022-04-22 19:00:24 +0530

    commiting files

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    late WebViewController controller;
+    return MaterialApp(
+      title: "Theemall",
+      home: WillPopScope(
+        onWillPop: () async {
+          String? url = await controller.currentUrl();
+          if (url == "https://theemall.probsoltechnology.in") {
+            return true;
+          } else {
+            controller.goBack();
+            return false;
+          }
+        },
+        child: Scaffold(
+          //below start code of app bar i.e only for ios build
+          appBar: AppBar(
+            title: Text('Theemall'),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () async {
+                  controller.goBack();
+                },
+              )
+            ],
+            backgroundColor: Colors.transparent,
+          ),
+          // app bar code ends
+          body: Container(
+            child: SafeArea(
+              child: WebView(
+                initialUrl: 'https://theemall.probsoltechnology.in',
+                javascriptMode: JavascriptMode.unrestricted,
+                onWebViewCreated: (WebViewController wc) {
+                  controller = wc;
+                },
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
